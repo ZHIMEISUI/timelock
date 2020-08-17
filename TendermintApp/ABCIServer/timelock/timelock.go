6af8843a120ae8363c7b750b4da15781547bdc56
Zhimei Sui <zhimei.sui1@monash.edu>
2020-08-17 21:21:45 +1000

    minor revise...

--- TendermintApp/ABCIServer/timelock/timelock.go
+++ TendermintApp/ABCIServer/timelock/timelock.go
@@ -7,7 +7,7 @@ import (
 	"github.com/tendermint/tendermint/abci/types"
 	"github.com/tendermint/tendermint/abci/example/code"
 )
-// test
+
 var _ types.Application = (*TimelockApplication)(nil)
 
 type TimelockApplication struct {
