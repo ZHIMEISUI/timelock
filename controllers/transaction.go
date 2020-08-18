0bcbfbca11eb1ed0d0d55ce8e3702de3579c4b1b
Zhimei Sui <zhimei.sui1@monash.edu>
2020-08-19 08:56:52 +1000

    fixing bugs...

--- controllers/transaction.go
+++ controllers/transaction.go
@@ -7,7 +7,7 @@ Transaction
 */
 type Transaction struct {
 	ID      int64   `bson:"id" json:"id"`
-	Flag string `bson:"flag" json:"flag"`
+	const Flag string `bson:"flag" json:"flag"`
 	CurrentTime uint8 `bson:"currenttime" json:"currenttime"`
 	From    string  `bson:"from" json:"from"`
 	To      string  `bson:"to" json:"to"`
@@ -33,28 +33,28 @@ Create :creating transactions
 */
 func (t *Transaction) Create() (bool, error) {
 	t.ID, _ = lib.GetNewUID()
-	const t.Flag = "Transaction"
+	t.Flag = "Transaction"
 	lib.Log.Debug("Create Transaction:", t)
 	return true, nil
 }
 
 func (t *FundingTransaction) CreateFundingTx() (bool, error) {
 	t.ID, _ = lib.GetNewUID()
-	const t.Flag = "FundingTx"
+	t.Flag = "FundingTx"
 	lib.Log.Debug("Create Funding Transaction:", t)
 	return true, nil
 }
 
 func (t *TriggerTransaction) CreateTriggerTx() (bool, error) {
 	t.ID, _ = lib.GetNewUID()
-	const t.Flag = "TriggerTx"
+	t.Flag = "TriggerTx"
 	lib.Log.Debug("Create Trigger Transaction:", t)
 	return true, nil
 }
 
 func (t *SettlementTransaction) CreateSettlementTx() (bool, error) {
 	t.ID, _ = lib.GetNewUID()
-	const t.Flag = "SettlementTx"
+	t.Flag = "SettlementTx"
 	lib.Log.Debug("Create Settlement Transaction:", t)
 	return true, nil
 }
