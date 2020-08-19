53d212bdd3d5c43c8b276a569f9ae522145bbd6e
Zhimei Sui <zhimei.sui1@monash.edu>
2020-08-19 20:48:17 +1000

    debug...

--- TendermintApp/ABCIServer/timelock/timelock.go
+++ TendermintApp/ABCIServer/timelock/timelock.go
@@ -18,7 +18,12 @@ import (
 func FundingTxVerify(tx map[string]string) bool {
 	if tx["Flag"] == "FundingTx"{
 		coin,_ := strconv.Atoi(tx["Coin"])
-		if coin <= 0 {return false}
+		if coin <= 0 {
+			lib.Log.Warning("Transaction is not valid")
+			lib.Log.Warning(tx["From"]+" deposits coin is: "+ tx["Coin"]
+			+". The expected deposit coin in Funding Transaction is higher than 0.")
+			return false
+		}
 		return true
 	}
 	return false
@@ -66,6 +71,8 @@ func (app *TimelockApplication) DeliverTx(req types.RequestDeliverTx) types.Resp
 	txhandle := strings.Replace(string(req.Tx), "'", "", -1)
 	txhandle = strings.Replace(string(txhandle), "{", "", -1)
 	txhandle = strings.Replace(string(txhandle), "[", "", -1)
+	txhandle = strings.Replace(string(txhandle), "]", "", -1)
+	txhandle = strings.Replace(string(txhandle), "}", "", -1)
 	lib.Log.Debug(txhandle)
 	txs := strings.Split(string(txhandle), ",")
 	txmap := make(map[string]string)
@@ -74,6 +81,10 @@ func (app *TimelockApplication) DeliverTx(req types.RequestDeliverTx) types.Resp
 		txmap[tsplit[0]] = tsplit[1]
 	}
 	if txmap["Flag"] == "FundingTx" {
+		if !FundingTxVerify(txmap) {
+			lib.Log.Warning("Code: "+code.CodeTypeBadNonce)
+			return types.ResponseDeliverTx{Code: code.CodeTypeBadNonce}
+		}
 		lib.Log.Debug("Transaction ID: "+txmap["ID"])
 		lib.Log.Debug("Transaction Type: "+txmap["Flag"])
 		lib.Log.Debug("Current Time: "+txmap["CurrentTime"])
@@ -81,7 +92,6 @@ func (app *TimelockApplication) DeliverTx(req types.RequestDeliverTx) types.Resp
 		lib.Log.Debug("Deposit Coins: "+txmap["Coin"])
 		lib.Log.Debug("Channel Version: "+txmap["NCommit"])
 		lib.Log.Debug("Sig: "+txmap["Sig"])
-		if !FundingTxVerify(txmap) {return types.ResponseDeliverTx{Code: code.CodeTypeBadNonce}}
 		return types.ResponseDeliverTx{Code: code.CodeTypeOK}
 	} else if txmap["Flag"] == "TriggerTx" {
 		lib.Log.Debug("Transaction ID: "+txmap["ID"])
