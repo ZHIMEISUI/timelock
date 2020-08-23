81508cebe2330d158585140e76f6e5477ff3044b
Zhimei Sui <zhimei.sui1@monash.edu>
2020-08-23 11:09:56 +1000

    fixing bugs..

--- TendermintApp/ABCIServer/timelock/timelock.go
+++ TendermintApp/ABCIServer/timelock/timelock.go
@@ -158,7 +158,7 @@ type TimelockApplication struct {
 	state State
 }
 
-func NewTimelockApplication(memDB dbm.DB.MemDB) *TimelockApplication {
+func NewTimelockApplication(memDB dbm.DB) *TimelockApplication {
 	lib.Log.Debug("NewTimelockApplication")
 	state := loadState(memDB)
 	return &TimelockApplication{state: state}
