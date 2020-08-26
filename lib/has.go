00d7317e83955937fc11fd471a3caefdb9597872
Zhimei Sui <zhimei.sui1@monash.edu>
2020-08-26 12:22:11 +1000

    fixing bugs...

--- lib/has.go
+++ lib/has.go
@@ -3,7 +3,7 @@ package lib
 
 func Has(strs []string, str string, index string) (string, bool) {
 	for _,t := range strs{
-		txmap := txHandle(t)
+		txmap := TxHandle(t)
 		if str == txmap[index] {
 			return t, true
 		}
